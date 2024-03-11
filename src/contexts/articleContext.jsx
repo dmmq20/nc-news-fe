d4333efaeadfcef3c1fd949a567ad916edcef206
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-03-11 14:15:37 +0000

    added articles and articlecard components and rendered articles on home page

---
+++ src/contexts/articleContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+export const ArticleContext = createContext();
+
+export const ArticleProvider = ({ children }) => {
+  const [articles, setArticles] = useState([]);
+
+  return (
+    <ArticleContext.Provider
+      value={{
+        articles,
+        setArticles,
+      }}
+    >
+      {children}
+    </ArticleContext.Provider>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ArticleProvider } from "./contexts/articleContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <ArticleProvider>
     <App />
-  </React.StrictMode>
+  </ArticleProvider>
 );
