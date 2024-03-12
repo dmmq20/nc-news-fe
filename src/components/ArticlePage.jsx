67b1999791b9b34ae3d91a3ec0e9bc42e8573218
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-03-12 09:14:22 +0000

    users can up/down vote an article

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { getArticle } from "../api";
+import { getArticle, updateArticleVotes } from "../api";
 import { useEffect, useState } from "react";
 import "./ArticlePage.css";
 import Comments from "./Comments";
@@ -9,6 +9,7 @@ const ArticlePage = () => {
   const { article_id } = useParams();
   const [article, setArticle] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
     setIsLoaded(false);
@@ -18,6 +19,21 @@ const ArticlePage = () => {
     });
   }, []);
 
+  const handleVote = (num) => {
+    const updatedArticle = { ...article, votes: article.votes + num };
+    setArticle(updatedArticle);
+    updateArticleVotes(article_id, { inc_votes: num })
+      .then((_) => {
+        setNotification("Thanks for voting!");
+        setTimeout(() => setNotification(null), 2500);
+      })
+      .catch((_) => {
+        setArticle({ ...article });
+        setNotification("Something went wrong...");
+        setTimeout(() => setNotification(null), 2500);
+      });
+  };
+
   return isLoaded ? (
     <article className="article-page">
       <img src={article.article_img_url} alt={article.title} />
@@ -25,7 +41,18 @@ const ArticlePage = () => {
       <div className="author-info">{article.author}</div>
       <p className="date">{new Date(article.created_at).toDateString()}</p>
       <p className="article-content">{article.body}</p>
-      <p className="likes">Likes: {article.votes}</p>
+      <div className="likes">
+        <p>Likes: {article.votes}</p>
+        <p>{notification}</p>
+        <div className="like-icons">
+          {/* TODO: give credit: https://icons8.com/license */}
+          <img onClick={() => handleVote(1)} src="/src/assets/thumbs-up.png" />
+          <img
+            onClick={() => handleVote(-1)}
+            src="/src/assets/thumbs-down.png"
+          />
+        </div>
+      </div>
       <Comments article_id={article_id} />
     </article>
   ) : (
