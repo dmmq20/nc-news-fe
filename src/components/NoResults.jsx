e55049b7c1757e6d7fe33fabf5b4373cfc099afa
dmmq20 <152389701+dmmq20@users.noreply.github.com>
2024-03-15 14:29:50 +0000

    added suggestions to noresults page and minor changes to socials styling

--- src/components/NoResults.jsx
+++ src/components/NoResults.jsx
@@ -1,8 +1,12 @@
+import Topics from "./Topics";
+
 const NoResults = () => {
   return (
     <div style={{ margin: "50% 0", width: "300px" }}>
       <h1>No results</h1>
       <p>Sorry, we couldn't find any articles matching your search criteria.</p>
+      <p>Why not check out one of the following:</p>
+      <Topics />
     </div>
   );
 };
